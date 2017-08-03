2535c92a4587c3f22fd68127de48f51ba1d27866
zhouliang6 <18510971680@163.com>
2017-08-03 11:33:58 +0800

    Initial commit

---
+++ app/src/main/java/com/zhouliang/utils/MainActivity.java
@@ -0,0 +1,29 @@
+package com.zhouliang.utils;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import com.zhouliang.utils.toastutils.ToastUtils;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.main_bt)
+    Button mainBt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.main_bt)
+    public void onViewClicked() {
+        ToastUtils.getIntance().showToast(MainActivity.this,"wo shi tost",0);
+    }
+}

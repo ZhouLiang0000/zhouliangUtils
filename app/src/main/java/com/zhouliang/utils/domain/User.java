b67fd0f97c20193b6f30d483512c6cd961b4391d
zhouliang6 <18510971680@163.com>
2017-08-09 18:40:29 +0800

    toast自定义

--- app/src/main/java/com/zhouliang/utils/domain/User.java
+++ app/src/main/java/com/zhouliang/utils/domain/User.java
@@ -1,12 +1,29 @@
 package com.zhouliang.utils.domain;
 
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Generated;
+import org.greenrobot.greendao.annotation.Id;
+
 /**
  * Created by zhouliang on 2017/8/9.
  */
-
+@Entity
 public class User {
+    @Id(autoincrement = true)
+    private Long id;
     private String userName;
     private String userGender;
+
+    @Generated(hash = 1374613186)
+    public User(Long id, String userName, String userGender) {
+        this.id = id;
+        this.userName = userName;
+        this.userGender = userGender;
+    }
+
+    @Generated(hash = 586692638)
+    public User() {
+    }
 
     public String getUserName() {
         return userName;
@@ -30,5 +47,13 @@ public class User {
                 "userName='" + userName + '\'' +
                 ", userGender='" + userGender + '\'' +
                 '}';
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }

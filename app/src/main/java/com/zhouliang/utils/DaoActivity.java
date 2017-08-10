fef77ee04abd3e9a93de68796d8c8b74fe9f4a9f
zhouliang6 <18510971680@163.com>
2017-08-10 15:13:41 +0800

    toast自定义

--- app/src/main/java/com/zhouliang/utils/DaoActivity.java
+++ app/src/main/java/com/zhouliang/utils/DaoActivity.java
@@ -4,14 +4,51 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.zhouliang.utils.base.UtilsActivity;
+import com.zhouliang.utils.base.UtilsApplication;
+import com.zhouliang.utils.dao.UserDao;
+import com.zhouliang.utils.domain.User;
+
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by zhouliang on 2017/8/10.
  */
 
 public class DaoActivity extends UtilsActivity {
+    @Bind(R.id.name_et)
+    EditText nameEt;
+    @Bind(R.id.gender_et)
+    EditText genderEt;
+    @Bind(R.id.age_et)
+    EditText ageEt;
+    @Bind(R.id.add_bt)
+    Button addBt;
+    @Bind(R.id.delete_bt)
+    Button deleteBt;
+    @Bind(R.id.update_bt)
+    Button updateBt;
+    @Bind(R.id.query_bt)
+    Button queryBt;
+    @Bind(R.id.query_all_bt)
+    Button queryAllBt;
+    @Bind(R.id.dao_recyclerview)
+    RecyclerView daoRecyclerview;
+    private UtilsApplication mUtilsApplication;
+    private UserDao mUserDao;
+    private List<User> mUserList;
+    private User mUser;
+    private String userName, userAge, userGender;
+
     public static void skipToDaoActivity(Context context) {
         Intent intent = new Intent(context, DaoActivity.class);
         context.startActivity(intent);
@@ -21,5 +58,76 @@ public class DaoActivity extends UtilsActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dao);
+        ButterKnife.bind(this);
+        initData();
+        initRecyclerView();
+    }
+
+    private void initData() {
+        mUtilsApplication = UtilsApplication.getUtilsApplication();
+        mUserDao = mUtilsApplication.getDaoSession().getUserDao();
+    }
+
+    private void initRecyclerView() {
+
+    }
+
+    @OnClick({R.id.add_bt, R.id.delete_bt, R.id.update_bt, R.id.query_bt, R.id.query_all_bt})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.add_bt:
+                cleanUserMessage();
+                insertData();
+                break;
+            case R.id.delete_bt:
+                cleanUserMessage();
+                deleteData(1);
+                break;
+            case R.id.update_bt:
+                cleanUserMessage();
+                updateData();
+                break;
+            case R.id.query_bt:
+                cleanUserMessage();
+                queryData(1);
+                break;
+            case R.id.query_all_bt:
+                cleanUserMessage();
+                queryAllData();
+                break;
+        }
+    }
+
+    private void getUserMessage() {
+        userName = nameEt.getText().toString().trim();
+        userAge = ageEt.getText().toString().trim();
+        userGender = genderEt.getText().toString().trim();
+    }
+    private void cleanUserMessage(){
+        nameEt.setText("");
+        ageEt.setText("");
+        genderEt.setText("");
+    }
+
+    private void insertData() {
+        getUserMessage();
+        User insertData = new User(null, userName, userGender, userAge);
+        mUserDao.insert(insertData);
+    }
+
+    private void deleteData(long id) {
+        mUserDao.deleteByKey(id);
+    }
+
+    private void updateData() {
+
+    }
+
+    private User queryData(long id) {
+        return mUserDao.load(id);
+    }
+
+    private List<User> queryAllData() {
+        return mUserDao.loadAll();
     }
 }

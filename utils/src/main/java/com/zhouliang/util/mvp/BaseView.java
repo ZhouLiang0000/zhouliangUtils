7876a97d036c14eaa82a446ef5b0f51dac9c1bc5
zhouliang6 <18510971680@163.com>
2017-10-24 15:23:12 +0800

    添加mvp的基础类定义

--- utils/src/main/java/com/zhouliang/util/mvp/BaseView.java
+++ utils/src/main/java/com/zhouliang/util/mvp/BaseView.java
@@ -5,4 +5,25 @@ package com.zhouliang.util.mvp;
  */
 
 public interface BaseView {
+    /**
+     * 设置toolbar,也可以不实现这个方法
+     */
+    void initToolbar();
+
+    /**
+     * 显示加载进度条
+     *
+     * @param content 进度条内容，如果为null则显示默认内容
+     */
+    void showLoadingDialog(String content);
+
+    /**
+     * 隐藏加载进度条
+     */
+    void dismissLoadingDialog();
+
+    /**
+     * 界面结束
+     */
+    void finishActivity();
 }
